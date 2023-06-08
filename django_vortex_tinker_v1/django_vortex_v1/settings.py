37716e22d6a9ed7bcc75d2931301b61ae1014519
Jonas Lekgau <jonaslekgau@gmail.com>
2023-06-08 15:49:20 +0200

    retry deployment

--- django_vortex_tinker_v1/django_vortex_v1/settings.py
+++ django_vortex_tinker_v1/django_vortex_v1/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'vortex_tinker',
+    'vortex_tinker',
     'vortex_tinker.apps.VortexTinkerConfig',
 ]
 
