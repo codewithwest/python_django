d17600533de5be841126224c4b66d13141372e21
codewithwest <jonaslekgau@gmail.com>
2023-06-20 12:47:32 +0200

    .

--- python_knight/python_knight/settings.py
+++ python_knight/python_knight/settings.py
@@ -67,17 +67,17 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "python_knight.wsgi.application"
+WSGI_APPLICATION = "python_knight.wsgi.app"
 
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
 }
 
 
